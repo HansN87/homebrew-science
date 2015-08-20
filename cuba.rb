16e83a7428af81f1b2d8a429418c3be15b140ca0
HansN87 <hans.niederhausen@stonybrook.edu>
2015-08-20 12:27:05 -0400

    Update cuba.rb

--- cuba.rb
+++ cuba.rb
@@ -2,15 +2,8 @@ require "formula"
 
 class Cuba < Formula
   homepage "http://www.feynarts.de/cuba"
-  url "http://www.feynarts.de/cuba/Cuba-4.1.tar.gz"
-  sha1 "2415fb0d29e06a9b86eadb7750fe79bc77dd44c0"
-
-  bottle do
-    cellar :any
-    sha1 "ab45cb4ad7dee2a0ef6e6fb95b515dbee2ea8f6e" => :yosemite
-    sha1 "0f2d8087ebe59c0d345b4aafa457023419e1656a" => :mavericks
-    sha1 "3ff524e9b690162decc9dd89e163a42ca035f684" => :mountain_lion
-  end
+  url "http://www.feynarts.de/cuba/Cuba-4.0.tar.gz"
+  sha1 "2e98480da9d4a9a222fa6085738e46cfbe217e68"
 
   option "without-check", "Skip build-time tests (not recommended)"
 
